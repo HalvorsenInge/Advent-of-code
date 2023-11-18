cfc10a712d4b723680d8c1be08924b5fb633181d
Snowsock <halvorsen.inge@gmail.com>
2023-11-18 20:00:32 +0100

    solved 1.2

--- 2015/1/day1.py
+++ 2015/1/day1.py
@@ -1,8 +1,19 @@
+import os
 from icecream import ic
+
+def list_files_in_current_directory():
+    current_directory = os.getcwd()
+    files = os.listdir(current_directory)
+    return files
+
 
 def parseInput(input:str):
     floor = 0
+    increment = 0
     for characther in input:
+        if floor == -1:
+            break
+        increment += 1
         if characther == '(':
             ic("incremnent floor")
             floor += 1
@@ -11,15 +22,24 @@ def parseInput(input:str):
             floor -= 1
         else:
             print("Invalid char")
-    return floor
+    return floor, increment
+
 
 def main():
+    filePath = "input"
+    ic(os.path.exists(filePath))
     try:
-        with open("input") as f:
+        with open(filePath) as f:
             ic(parseInput(f.read()))
     except FileNotFoundError:
         print("File not found.")
     except IOError:
         print("Error reading the file.")
+
+
 if __name__ == "__main__":
+    files_in_current_directory = list_files_in_current_directory()
+    print("Files in the current directory:")
+    for file in files_in_current_directory:
+        print(file)
     main()   
