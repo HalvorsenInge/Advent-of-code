5417b72cf758552bcda241d09fdf5f1d2280bf16
Snowsock <halvorsen.inge@gmail.com>
2023-11-20 20:49:26 +0100

    solves day 2.2

--- 2015/2/day2.py
+++ 2015/2/day2.py
@@ -1,17 +1,24 @@
 from icecream import ic
 
 def parseInput(input:str):
-    inputArray = [formula.split('x') for formula in input.splitlines()]
-    acc = 0
-    for formula in inputArray:
-        acc += calculatePaper(formula)
-    ic(acc)
+    paper_array = [formula.split('x') for formula in input.splitlines()]
+    paper = 0
+    ribbon = 0
+    for formula in paper_array:
+        paper += calculatePaper(formula)
+        ribbon += calculateRibbon(formula)
+    ic(paper)
+    ic(ribbon)
     
 def calculatePaper(arr):
     return_array = sorted([2*int(arr[0])*int(arr[1]),
                     2*int(arr[1])*int(arr[2]),
                     2*int(arr[2])*int(arr[0])])
     return return_array[0]/2+return_array[0]+return_array[1]+return_array[2]
+
+def calculateRibbon(arr):
+    return_array = sorted(list(map(int, arr)))
+    return return_array[0]*2+return_array[1]*2+return_array[0]*return_array[1]*return_array[2]
 
 def main():
     f = open("input")
